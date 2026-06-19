b0a1cb44100b8b27366ea70a3c0eb51d220565d1
Eduardo Achar <eduardoachar1717@gmail.com>
2026-06-19 17:50:45 -0300

    correcao de vulnerabilidades no script Terraform

--- infra/main.tf
+++ infra/main.tf
@@ -14,29 +14,30 @@ provider "aws" {
 # Criação de um Grupo de Segurança (Firewall)
 resource "aws_security_group" "helpdesk_sg" {
   name        = "helpdesk_sg"
-  description = "Permite trafego web para o Helpdesk"
+  description = "Permite trafego web para o Helpdesk restrito a Intranet"
 
   ingress {
-    description = "HTTP do Front-end"
+    description = "HTTP do Front-end (Restrito a Rede Interna)"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/8"] # Correção CRITICAL: Restrito à intranet
   }
 
   ingress {
-    description = "Acesso a API (Backend)"
+    description = "Acesso a API (Restrito a Rede Interna)"
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/8"] # Correção CRITICAL: Restrito à intranet
   }
 
   egress {
+    description = "Saida controlada para a Rede Interna" # Correção LOW: Adicionada descrição
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/8"] # Correção CRITICAL: Evita vazamento de dados para a internet externa
   }
 }
 
@@ -46,6 +47,16 @@ resource "aws_instance" "helpdesk_server" {
   instance_type = "t2.micro"
   
   vpc_security_group_ids = [aws_security_group.helpdesk_sg.id]
+
+  # Correção HIGH: Criptografia ativada no disco raiz
+  root_block_device {
+    encrypted = true
+  }
+
+  # Correção HIGH: Exige token de segurança para acessar os metadados da instância
+  metadata_options {
+    http_tokens = "required"
+  }
 
   tags = {
     Name        = "Helpdesk-Production-Server"
